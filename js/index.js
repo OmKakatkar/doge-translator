3065bb12f72dcffc30b6cac00432aa2016723714
Om Kakatkar <omkakatkar@gmail.com>
2021-04-14 12:56:46 +0530

    site complete, ready to deploy

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let inputTxt = document.querySelector('#input-txt');
+let translate = document.querySelector('#translate');
+let outputTxt = document.querySelector('#output-txt');
+
+let url = `https://api.funtranslations.com/translate/doge.json`;
+
+translate.addEventListener("click", clickEventHandler);
+
+function getUrl(text)
+{
+    return `${url}?text=${text}`;
+}
+
+function clickEventHandler()
+{
+    // console.log("Clicked");
+    fetch(getUrl(inputTxt.value))
+        .then(response => response.json())
+        .then(json => 
+        {
+            let output = json.contents.translated;
+            outputTxt.innerText = output;
+        })
+}
