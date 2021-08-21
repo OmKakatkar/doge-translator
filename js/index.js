04d6793f34e3f3ffb1e1f1bd6fc7cd97a26c26cb
Om Kakatkar <omkakatkar@gmail.com>
2021-08-21 18:51:05 +0530

    added error handling

--- js/index.js
+++ js/index.js
@@ -1,24 +1,27 @@
-let inputTxt = document.querySelector('#input-txt');
-let translate = document.querySelector('#translate');
-let outputTxt = document.querySelector('#output-txt');
+let inputTxt = document.querySelector("#input-txt");
+let translate = document.querySelector("#translate");
+let outputTxt = document.querySelector("#output-txt");
 
 let url = `https://api.funtranslations.com/translate/doge.json`;
 
 translate.addEventListener("click", clickEventHandler);
 
-function getUrl(text)
-{
-    return `${url}?text=${text}`;
+function getUrl(text) {
+  return `${url}?text=${text}`;
 }
 
-function clickEventHandler()
-{
-    // console.log("Clicked");
-    fetch(getUrl(inputTxt.value))
-        .then(response => response.json())
-        .then(json => 
-        {
-            let output = json.contents.translated;
-            outputTxt.innerText = output;
-        })
+function clickEventHandler() {
+  // console.log("Clicked");
+  fetch(getUrl(inputTxt.value))
+    .then((response) => response.json())
+    .then((json) => {
+      let output = json.contents.translated;
+      outputTxt.innerText = output;
+    })
+    .catch(errorHandler);
+}
+
+function errorHandler(error) {
+  console.log("An error occurred", error);
+  alert("Something went wrong, please try again later");
 }
